4e3884f983f76794e3054a64658402c07f63c0fb
krsh <kiavash86habibi@gmail.com>
2026-05-24 01:14:08 +0330

    adding 3 property of name and howMany and health

---
+++ model/card.h
@@ -0,0 +1,14 @@
+#include <string>
+class card
+{
+private:
+std::string name{};
+int howManyRoundNeededForSpecialPower{};
+int health{};
+//pair<func> abilities;
+//func specialAbility
+
+public:
+    card();
+    ~card();
+};
