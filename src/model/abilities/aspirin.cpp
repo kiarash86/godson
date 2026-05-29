2ec63701856bbd431c94983ddcfe249803ccf8b6
krsh <kiavash86habibi@gmail.com>
2026-05-29 18:35:52 +0330

    deleting override from func excute

--- src/model/abilities/aspirin.cpp
+++ src/model/abilities/aspirin.cpp
@@ -13,14 +13,14 @@ void aspirin::attackChosenEnemy(card *enemy)
 {
     enemy->damage(40 * owner->getBuffDmg());
 }
-void healChosenTeammate(card * teammate)
+void aspirin::healChosenTeammate(card * teammate)
 {
     teammate->heal(40);
 }
 
 aspirin::aspirin(card *owner) : ability( owner, 3) {};
 
-void aspirin::excute(std::vector<card *>& Team, std::vector<card *>& enemy, int tagetIndex) override
+void aspirin::excute(std::vector<card *>& Team, std::vector<card *>& enemy, int tagetIndex) 
 {
     attackChosenEnemy(enemy[tagetIndex]);
     healChosenTeammate(chooseRandomTeammate(Team));
