e346e1020375e9fbe5f9234504584bbba07e04e5
krsh <kiavash86habibi@gmail.com>
2026-05-29 23:22:03 +0330

    adding buffShield and using it in func of attack

--- include/model/cards/card.h
+++ include/model/cards/card.h
@@ -8,6 +8,7 @@ int health{};
 int maxHealth{};
 std::pair<float , int> buffDmg = {1 , 15};
 std::pair<int , int> buffHeal = {0 , 15};
+std::pair<int , int> buffShield = {0 , 0};
 //can be stacked buff dmgs??? i mean 2 times 1.2 or reset to 1.2?
 //TODO
 
@@ -18,7 +19,7 @@ public:
     card(const std::string &name, const int &health, const int &howManyRound, const std::string &type) : name(name), health(health), maxHealth(health), howManyRoundNeededForSpecialPower(howManyRound) , type(type) {};
     ~card();
     void addRound();
-    void damage(const int&);
+    void damage( int&);
     void heal(const int&);
     bool isDead();
 
@@ -27,4 +28,6 @@ public:
     void setBuffDmg(const int &buff , const int &howManyRound){buffDmg = {buff , howManyRound};};
     int getBuffHeal() const{ return buffHeal.first;};
     void setBuffHeal(const int &buff , const int &howManyRound){buffHeal = {buff , howManyRound};};
+    int getBuffShield() const{ return buffShield.first;};
+    void setBuffShield(const int &buff , const int &howManyRound){buffShield = {buff , howManyRound};};
 };
--- src/model/cards/card.cpp
+++ src/model/cards/card.cpp
@@ -12,8 +12,20 @@ bool card::isDead()
     return health;
 }
 
-void card::damage(const int& dmg)
+void card::damage( int& dmg)
 {
+    if (buffShield.first!=0 and buffShield.second!=0)
+    {
+        if (buffShield.first > dmg)
+        {
+            buffShield.first -= dmg;
+            dmg =0 ;
+        }
+        else {
+            dmg -=buffShield.first;
+            buffShield.first =0 ;
+        }
+    }
     health = (health - dmg < 0) ? 0: health - dmg;
 }
 void card::heal(const int& heal)
