8d25825ceb6d3035277b1becedfc7075a673642b
krsh <kiavash86habibi@gmail.com>
2026-05-30 13:16:05 +0330

    adding some func that are incompleted for see the results of view

--- include/controller/gameManager.h
+++ include/controller/gameManager.h
@@ -1,16 +1,60 @@
+#include "../view/helpMeView.h"
+#include "../view/startView.h"
+#include "../view/storitesMenu.h"
+#include "../view/storyShow.h"
+
 class gameManager
 {
 private:
-    
 public:
-    gameManager();
-    ~gameManager();
+
+void whereToGoFromStoriesMenu()
+{
+    int index;
+    while (true)
+    {
+        index = ShowStoriesMenuOption();
+        if (index==7)
+        {
+            break;
+        }
+        //TODO 
+        //adding a trick for exiting to main menu
+        //i think its wrong but better then nothing now
+        handleStoriesMenu(index);
+        
+    }
+    
+  
+    //TODO 
+    //should be fixed later because of stacking func in func
+whereToGoFromMainMenu();
+
+    
+}
+void whereToGoFromMainMenu()
+{
+    int index;
+    index=ShowStartMenuOption();
+    switch (index)
+    {
+        case 0:
+        
+        break;
+        case 1:
+        
+        break;
+        case 2:
+        whereToGoFromStoriesMenu();
+        break;
+        case 3:
+        exit(0);
+        break;
+        
+        default:
+        break;
+    }
+}
+
+
 };
-
-gameManager::gameManager()
-{
-}
-
-gameManager::~gameManager()
-{
-}
