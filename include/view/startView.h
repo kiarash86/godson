e3c5baf18a24be53cd12b4c2a50a555ce6a060a7
krsh <kiavash86habibi@gmail.com>
2026-05-30 10:30:15 +0330

    adding startMenu

---
+++ include/view/startView.h
@@ -0,0 +1,13 @@
+#include <iostream>
+class startView
+{
+private:
+public:
+    void ShowMenuOption()
+    {
+        std::cout << "WELCOME TO THIS GAME!!!\n";
+        std::cout << "1) start new game\n";
+        std::cout << "2) help me\n";
+        std::cout << "3)story of cards\n";
+    }
+};
