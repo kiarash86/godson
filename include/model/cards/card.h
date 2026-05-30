4daf8c48b35a2ff9caf40860498809e5ea38e386
krsh <kiavash86habibi@gmail.com>
2026-05-30 09:27:22 +0330

    deleting pair buffs and replace them with int variables and changing the set and get funcs of them

--- include/model/cards/card.h
+++ include/model/cards/card.h
@@ -2,37 +2,44 @@
 class card
 {
 protected:
-std::string name{};
-int howManyRoundNeededForSpecialPower{};
-int health{};
-int maxHealth{};
-int hidden{};
+    std::string name{};
+    int howManyRoundNeededForSpecialPower{};
+    int health{};
+    int maxHealth{};
+    int hidden{};
 
-std::pair<float , int> buffDmg = {1 , 15};
-std::pair<int , int> buffHeal = {0 , 15};
-std::pair<int , int> buffShield = {0 , 0};
-//can be stacked buff dmgs??? i mean 2 times 1.2 or reset to 1.2?
-//TODO
+    int buffDmg{1};
+    int buffShield{0};
+    // can be stacked buff dmgs??? i mean 2 times 1.2 or reset to 1.2?
+    // TODO
 
-std::string type;
-//func specialAbility
+    std::string type;
+    // func specialAbility
 
 public:
-    card(const std::string &name, const int &health, const int &howManyRound, const std::string &type) : name(name), health(health), maxHealth(health), howManyRoundNeededForSpecialPower(howManyRound) , type(type) {};
+    card(const std::string &name, const int &health, const int &howManyRound, const std::string &type) : name(name), health(health), maxHealth(health), howManyRoundNeededForSpecialPower(howManyRound), type(type) {};
     ~card();
     void dmgWithBomb(const int &);
     void addRound();
-    void damage(const int&);
-    void heal(const int&);
+    void damage(const int &);
+    void heal(const int &);
     bool isDead();
     bool canBeKilledWithThisShot(const int);
-    int getHealth() const{ return health;};
-    int getBuffDmg() const{ return buffDmg.first;};
-    void setBuffDmg(const int &buff , const int &howManyRound){buffDmg = {buff , howManyRound};};
-    int getBuffHeal() const{ return buffHeal.first;};
-    void setBuffHeal(const int &buff , const int &howManyRound){buffHeal = {buff , howManyRound};};
-    int getBuffShield() const{ return buffShield.first;};
-    void setBuffShield(const int &buff , const int &howManyRound){buffShield = {buff , howManyRound};};
-    bool IsHidden() const{ return hidden;};
-    void increaseHiddenTime(const int &buff ){hidden += buff ;};
+    int getHealth() const { return health; };
+    int getBuffDmg() const { return buffDmg; };
+    void increaseBuffDmg(const int &buff) { buffDmg *= buff; };
+    void decreaseBuffDmg(const int &buff) { buffDmg /= buff; };
+    int getBuffShield() const { return buffShield; };
+    void increaseBuffShield(const int &buff) { buffShield += buff; };
+    void decreaseBuffShield(const int &buff)
+    {
+        if (buff >= buffShield)
+        {
+            buffShield = 0;
+            return;
+        }
+        buffShield -= buff;
+    };
+    bool IsHidden() const { return hidden; };
+    void increaseHiddenTime(const int &buff) { hidden += buff; };
 };
