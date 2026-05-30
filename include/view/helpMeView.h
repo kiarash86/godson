7634a716c574eb86261ef58a6ef9a53e347670e0
krsh <kiavash86habibi@gmail.com>
2026-05-30 10:40:50 +0330

    a basic class for Help page

---
+++ include/view/helpMeView.h
@@ -0,0 +1,12 @@
+#include<iostream>
+class helpMeView
+{
+private:
+    
+public:
+    void help()
+    {
+        std::cout << "this a game that u should...\n";
+        std::cout << "exit\n";
+    }
+};
