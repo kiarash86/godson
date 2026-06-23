c185b5059c96397078ea2eaca246c9197a93bdda
krsh <kiavash86habibi@gmail.com>
2026-06-23 11:11:06 +0330

    completing the heal effect

--- include/controller/healEffect.h
+++ include/controller/healEffect.h
@@ -1,22 +1,30 @@
+#pragma once
 #include "observerEffect.h"
-class healEffect : public observerEffect {
+
+class healEffect : public observerEffect
+{
 private:
-  int healAmount{};
+    int healAmount{};
 
 public:
-  // we use the buff of the time that bombed on target
-  healEffect(card *onWho, int healAmount, int howManyRound)
-      : observerEffect(onWho, howManyRound), healAmount(healAmount){};
-  void turnEnded(bool &reverseWorld) {
-    howManyRound--;
-    if (onWho->isDead()) {
-      return;
-    }
-    if (reverseWorld) {
-      onWho->damage(healAmount);
-    } else {
+    healEffect(card *onWho, int healAmount, int howManyRound)
+        : observerEffect(onWho, howManyRound), healAmount(healAmount) {}
 
-      onWho->heal(healAmount);
+    void turnEnded(bool &reverseWorld) override
+    {
+        --howManyRound;
+        if (onWho == nullptr || onWho->isDead())
+        {
+            return;
+        }
+
+        if (reverseWorld)
+        {
+            onWho->damage(healAmount);
+        }
+        else
+        {
+            onWho->heal(healAmount);
+        }
     }
-  }
 };
